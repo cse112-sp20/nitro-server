59c940503a6b1d343a2d1bf626dcc9624b377afa
Phuc Dao <39757882+Phuc-Dao@users.noreply.github.com>
2020-05-09 00:25:45 -0700

    Update app.py

--- app.py
+++ app.py
@@ -4,17 +4,19 @@ from flask_cors import CORS, cross_origin
 import requests, json
 
 # Enable Cors
+# We need this because the browser throws errors if we don't have it
 app = Flask(__name__)
 cors = CORS(app)
 app.config['CORS_HEADERS'] = 'Content-Type'
 
-#Connect to MongoDB
-client = MongoClient("mongodb://0.0.0.0:27017")
-db = client.testdb
-issues = db.test_collections
+#Connect to MongoDB instance
+#Note Mongo will create the database if it does not exist
+client = MongoClient("mongodb://0.0.0.0:27017") #Specify our host
+db = client.testdb #specify which db to use (in our case it is testdb)
+issues = db.test_collections #Within each db we have collections, choose the collection named 'test_collection'
 
 """
-imports issues by calling github api
+Client will call this endpoint, which will call another endpoint (github api) and return the result to the client
 """
 @app.route('/import', methods=['POST'])
 @cross_origin()
@@ -33,6 +35,7 @@ def run():
             return key == 'title' or key == 'updated_at' or key == 'id' or key == '_id';
         # insert element into the database
         issues.insert_one({key:val for key, val in elem.items() if is_title_id_updated(key)})
+    #Return JSON to the user
     return jsonify({"message": "ok"})
 
 @app.route('/', methods=['GET'])
@@ -63,6 +66,7 @@ delete from the database with a given id
 @app.route('/delete', methods=['POST'])
 @cross_origin()
 def deleteItem():
+    #Request.args are parameters that the client will send to us
     id = int(request.args.get('id'))
     print("deleteing with id {}".format(id))
     issues.delete_many({"id": id});

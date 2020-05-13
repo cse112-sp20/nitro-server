1bdf3b4c06c7d1a695ec7dab3f9396254b170a85
Phuc-Dao <pcdao@ucsd.edu>
2020-05-13 05:34:17 -0700

    finished the basecamp class

--- Basecamp.py
+++ Basecamp.py
@@ -1,55 +1,71 @@
-
-import requests
+#pylint: disable=invalid-name
+"""
+Module to interface with Basecamp api. Used to instantiate endpoints and token
+"""
 import json
-"""
-Helper methods to parse the basecamp api
-"""
+import requests
+
 class Basecamp:
+    """Object used to interact with basecamp api"""
     def __init__(self, auth_token, acc_id):
-        self.auth_token = auth_token
+    
+        # Set authentication parameters
+        self.auth_token = auth_token.decode()
         self.acc_id = acc_id
-        self.base_endpoint = "https://3.basecampapi.com/{}/projects.json".format(self.acc_id)
         self.header = {"Authorization": "Bearer " + self.auth_token}
-        r = requests.get(self.base_endpoint, headers=self.header)
-        if r.status_code != 200:
-            raise Exception("unabl to reach json api status code:{}".format(r.status_code))
-        else:
-            self.json_dump = r
-    """
-    Gets a dump of the json that for the front-endj
-    """
-    def jsonDump(self):
+
+        # Basecamp endpoints
+        self.base_endpoint = "https://3.basecampapi.com/{}/projects.json".format(self.acc_id)
+
+    def json_dump(self):
+        """
+        Gets a dump of the json that for the front-end
+        returns: dict
+        """
+        # Gets the parent object of basecamp to parse
+        project_response = requests.get(self.base_endpoint, headers=self.header)
+
+        if project_response.status_code != 200:
+            raise Exception("failed request")
+
+        project_json = json.loads(project_response.content)
         acc_obj = {}
         acc_obj['account_id'] = self.acc_id
-        #print(self.json_dump.content)
-        acc_obj['teams'] = self.getTeams()
+        acc_obj['teams'] = self.get_teams(project_json)
         return acc_obj
 
-    """
-    Returns a list of dictionary of team objects
-    """
-    def getTeams(self):
-        data = json.loads(self.json_dump.content)
+    def get_teams(self, project_json):
+        """
+        Gets all teams on basecamp
+        project_json: dict
+        returns: dictionary
+        """
         team_res = []
-        for projects in data:
-            if(projects['purpose'] == 'team'):
+        for projects in project_json:
+            if projects['purpose'] == 'team':
                 team = {}
                 team['name'] = projects['name']
                 team['project_id'] = projects['id']
-                team['todoset_id'] = [item['id'] for item in projects['dock'] if item['name'] == 'todoset' ][0]
-                team['task_list'] = self.getTaskList([item['url']  for item in projects['dock'] if item['name'] == 'todoset' ][0])
+                team['todoset_id'] = [item['id']
+                                      for item in projects['dock']
+                                      if item['name'] == 'todoset'][0]
+                team['task_list'] = self.get_task_list([item['url']
+                                                        for item in projects['dock']
+                                                        if item['name'] == 'todoset'][0])
+                team['points'] = self.calculate_points()
                 team_res.append(team)
         return team_res
 
-    """
-    Generate List of todolists
-    @todoset_endpoint: list of todoset url endpoints
-    """
-    def getTaskList(self, taskset_endpoint):
+    def get_task_list(self, taskset_endpoint):
+        """
+        Generate List of todolists
+        taskset_endpoint: utf-8 string of the taskset endpoint
+        """
         taskset_response = requests.get(taskset_endpoint, headers=self.header)
+
         if taskset_response.status_code != 200:
-            raise Exception("Failed to fetch task list at endpint {}".format(taskset_endpoint))    
-        
+            raise Exception("Failed to fetch task list at endpint {}".format(taskset_endpoint))
+
         #Each project has multiple todo lists
         result_list = []
         task_set_data = json.loads(taskset_response.content)
@@ -57,8 +73,10 @@ class Basecamp:
         # Get the task_lists from task_set url
         task_list_url = task_set_data['todolists_url']
         task_list_response = requests.get(task_list_url, headers=self.header)
-        assert(task_list_response.status_code == 200)
-        
+
+        if task_list_response.status_code != 200:
+            raise Exception("unable to get result")
+
         # Add tasklist objects
         for task_list in json.loads(task_list_response.content):
             task_list_elem = {}
@@ -67,15 +85,40 @@ class Basecamp:
             task_list_elem['task_list_name'] = task_list['name']
             task_list_elem['parent_id'] = task_list['parent']['id']
             task_list_elem['parent_project'] = task_list['bucket']['name']
-            task_list_elem['tasks'] = self.getTask(task_list_elem['todos_url'])
+            task_list_elem['task'] = self.get_task(task_list['todos_url'])
             result_list.append(task_list_elem)
         return result_list
 
-    """
-    Get the individual tasks for each task list
-    """
-    def getTask(self, todos_url):
-        print(todos_url)
-        return []
+    def get_task(self, todos_url):
+        """
+        Get the individual tasks for each task list
+        """
+        res = []
+        todo_response = requests.get(todos_url, headers=self.header)
+        if todo_response.status_code != 200:
+            raise Exception("Faled to get todo items")
+        todo_list = json.loads(todo_response.content)
+        for todo in todo_list:
+            task_item = {}
+            task_item['id'] = todo['id']
+            task_item['title'] = todo['title']
+            task_item['status'] = todo['status']
+            task_item['due_on'] = todo['due_on']
+            task_item['assignees'] = todo['assignees']
+            res.append(task_item)
+        return res
 
+    def complete_task(self, project_id, todo_id):
+        """
+        Makes a post request to Basecamp api to complete a todo
+        """
+        complete_todo_endpoint = 'https://3.basecampapi.com/{}/buckets/{}/todos/{}/completion.json'.format(self.acc_id, project_id, todo_id)
+        post_response = requests.post(complete_todo_endpoint, headers=self.header)
+        if post_response.status_code != 204:
+            raise Exception("bad requests")
+        return {"success" : "ok" }
         
+    @staticmethod
+    def calculate_points():
+        """ Cacluate the points for each team"""
+        return 100 + 10
--- app.py
+++ app.py
@@ -1,17 +1,18 @@
+"""
+Entry point for application
+"""
 import os
 import requests
 from flask import Flask, request, session, redirect, jsonify
 from dotenv import load_dotenv
 from Basecamp import Basecamp
 
-"""
-Load the enviornment variables and initialize flask object
-"""
+#Configurations
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'shh'
-load_dotenv() #Load enviornment variables
+load_dotenv() 
 
-# Enciornment Variables
+# Enviornment Variables
 CLIENT_ID = os.environ.get('CLIENT_ID')
 CLIENT_SECRET = os.environ.get('CLIENT_SECRET')
 REDIRECT_URI = os.environ.get('REDIRECT_URI')
@@ -22,37 +23,47 @@ Use this route to get redirected to basecamp
 """
 @app.route('/login')
 def login():
+    """
+    Logs the user in and stores a new authentication token in their session
+    """
     authorization_url = 'https://launchpad.37signals.com/authorization/new?type=web_server&client_id={}&redirect_uri={}'.format(CLIENT_ID, REDIRECT_URI)
     return redirect(authorization_url), 302
 
-@app.route('/task')
-def home():
-    return "hello" 
+@app.route('/tasks')
+def get_task():
+    if 'AUTH_TOKEN' not in session:
+        return redirect('/login')
+    basecamp = Basecamp(session.get('AUTH_TOKEN'), ACCOUNT_ID)
+    return jsonify(basecamp.json_dump()) 
 
-@app.route('/somename')
-def somefunct():
-    res = {"name" : "phuc"}
-    return jsonify(res)
+@app.route('/complete', methods=['POST', 'GET'])
+def complete_task():
+    if 'AUTH_TOKEN' not in session:
+        return redirect('/login')
 
+    # Get the id of the todo item we want to delete and the id of the project
+    todo_id = request.args.get('todo')
+    project_id = request.args.get('project')
+
+    # If either one is not give then return 400
+    if not todo_id or not project_id:
+        return "bad request", 400
+
+    basecamp = Basecamp(session.get('AUTH_TOKEN'), ACCOUNT_ID)
+    basecamp.complete_task(project_id, todo_id)
+    return "good", 200
 
 # Login route gets redirected here
 @app.route('/get_token', methods=['GET'])
 def get_Token():
-    #print(request)
     code = request.args.get('code')
     token_url = 'https://launchpad.37signals.com/authorization/token?type=web_server&client_id={}&redirect_uri={}&client_secret={}&code={}'.format(CLIENT_ID,REDIRECT_URI, CLIENT_SECRET, code)
     r = requests.post(token_url)
     if r.status_code == 200:
         token = r.json()['access_token'].encode('ascii', 'replace') #The Access token right here
-        """
-        #Make a request
-        url = 'https://3.basecampapi.com/4514340/projects.json'
-        print(token)
-        r = requests.get(url, headers={"Authorization": "Bearer " + token})
-        res = r.json()
-        """
-        basecamp = Basecamp(token, ACCOUNT_ID)
-        return jsonify(basecamp.jsonDump())
+        session['AUTH_TOKEN'] = token
+        base_camp = Basecamp(token, ACCOUNT_ID)
+        return jsonify(base_camp.json_dump())
     return "Failure"
 
 if __name__ == '__main__':
